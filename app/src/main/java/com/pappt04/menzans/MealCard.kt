92164092e19d880903e73fed56da2c18feb19d29
Pappt04 <69700151+Pappt04@users.noreply.github.com>
2024-09-07 15:45:54 +0200

    Added buttons to MealCard

--- app/src/main/java/com/pappt04/menzans/MealCard.kt
+++ app/src/main/java/com/pappt04/menzans/MealCard.kt
@@ -2,17 +2,31 @@ package com.pappt04.menzans
 
 import android.content.res.Configuration
 import android.view.Surface
+import android.widget.Space
+import android.widget.Toast
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,10 +38,14 @@ data class MealData(var name: String, var price: Int, var remaining: Int)
 
 @Composable
 fun MealCard(meal: MealData) {
+    var isExpanded by remember { mutableStateOf(false) }
+    val context= LocalContext.current
+
     Card(
         elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
         modifier = Modifier
             .padding(8.dp)
+            .clickable { isExpanded=!isExpanded }
     ) {
         Column {
             Text(
@@ -57,6 +75,28 @@ fun MealCard(meal: MealData) {
         )
         //TODO DAYS IN WEEK
         //TODO TWO BUTTONS TO ADD AND SUBTRACT
+        AnimatedVisibility(isExpanded,
+            modifier = Modifier
+                .animateContentSize(animationSpec = spring(dampingRatio = Spring.DampingRatioLowBouncy,
+                    stiffness = Spring.StiffnessLow))) {
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 20.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly) {
+                Button(onClick = {
+                    Toast.makeText(context, "Welcome to Geeks for Geeks", Toast.LENGTH_LONG).show()
+                },
+                   ) {
+                    Text("Minus")
+                }
+
+                Button(onClick = {
+                    Toast.makeText(context, "Welcome to Geeks for Geeks", Toast.LENGTH_LONG).show()
+                }) {
+                    Text("Plus")
+                }
+            }
+        }
     }
 }
 
--- app/src/main/java/com/pappt04/menzans/MealContainer.kt
+++ app/src/main/java/com/pappt04/menzans/MealContainer.kt
@@ -1,5 +1,6 @@
 package com.pappt04.menzans
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -7,6 +8,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.pappt04.menzans.ui.theme.MenzaNSTheme
 
@@ -17,9 +19,22 @@ fun MealContainer(meals: List<MealData>) {
             modifier = Modifier
                 .fillMaxWidth()
                 .fillMaxHeight()
-                .padding(8.dp)
+                .padding(10.dp)
         ) {
             items(meals) { meal: MealData -> MealCard(meal) }
         }
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMealContainer() {
+    MenzaNSTheme {
+        MealContainer(DummyData.MealSample)
     }
 }
